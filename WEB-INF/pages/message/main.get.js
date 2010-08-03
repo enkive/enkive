483550032725e4979aa104b7d471042b221190c7
Lee N Dobryden <lee@linuxbox.com>
2010-08-03 17:09:20 -0400

    Updating single message display

--- WEB-INF/pages/message/main.get.js
+++ WEB-INF/pages/message/main.get.js
@@ -2,4 +2,4 @@ var messageid = context.properties["messageid"];
 // get a connector to the Alfresco repository endpoint
 var connector = remote.connect("alfresco");
 // retrieve the web script index page 
-model.text = connector.get("/enkive/messages/" + messageid);
+model.text = connector.get("/enkive/message/" + messageid);
