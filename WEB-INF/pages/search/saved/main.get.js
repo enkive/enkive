96252c40e974c4a1785cbcf6aafa5c276b0b139b
Lee N Dobryden <lee@linuxbox.com>
2010-12-21 12:45:18 -0500

    Revert "Revert "Updating navigation sub-menu""
    This reverts commit 525bb9e3d0bb800ba3cbf261b816b9e124685cb1.

--- WEB-INF/pages/search/saved/main.get.js
+++
@@ -1,7 +0,0 @@
-// get a connector to the Alfresco repository endpoint
-var connector = remote.connect("alfresco");
-// retrieve the web script index page 
-var searchlist = connector.get("/enkive/search/saved");
-var searchJSON = jsonUtils.toObject(searchlist);
-
-model.searchList = eval("(" + searchJSON.searches + ")");
