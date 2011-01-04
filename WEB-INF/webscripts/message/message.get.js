6efabfcc4da78abd59eb8eb8461d8f12e152a86d
Lee N Dobryden <lee@linuxbox.com>
2011-01-04 10:54:10 -0500

    Add message to notify if session has timed out when trying to view a message

--- WEB-INF/webscripts/message/message.get.js
+++ WEB-INF/webscripts/message/message.get.js
@@ -1,5 +1,10 @@
 var messageid = context.properties["messageid"];
 // get a connector to the Alfresco repository endpoint
 var connector = remote.connect("alfresco");
-// retrieve the web script index page 
-model.text = connector.get("/enkive/message/" + messageid);
+// retrieve the message
+var response = connector.get("/enkive/message/" + messageid);
+
+if(response.status == 200)
+	model.text = response;
+else
+	model.text = "Your session has expired.  You must login to view this message";
