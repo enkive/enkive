24d8f0e560d00e652543e31c15708a4bf0a7f21f
Lee Dobryden <lee@linuxbox.com>
2011-04-20 15:07:13 -0400

    Adding notes to the MessageArchivingService interface

--- src/java/com/linuxbox/enkive/archiver/MessageArchivingService.java
+++ src/java/com/linuxbox/enkive/archiver/MessageArchivingService.java
@@ -1,14 +1,40 @@
 package com.linuxbox.enkive.archiver;
 
 import com.linuxbox.enkive.archiver.exceptions.CannotArchiveException;
+import com.linuxbox.enkive.docstore.exceptions.StorageException;
 import com.linuxbox.enkive.message.Message;
 
 public interface MessageArchivingService {
-	
+	/**
+	 * Stores the given message and generates a unique identifier for the
+	 * message, which is returned. If the message is already stored, it is not
+	 * stored a second time, but instead the existing identifier is returned.
+	 * This method does de-duplication.
+	 * 
+	 * @param message
+	 * @return unique identifier for message
+	 * @throws CannotArchiveException
+	 */
 	public String storeOrFindMessage(Message message) throws CannotArchiveException;
 	
+	/**
+	 * Stores the given message and generates a unique identifier for the
+	 * message, which is returned.
+	 * 
+	 * @param message
+	 * @return unique identifier for message
+	 * @throws CannotArchiveException
+	 */
 	public String storeMessage(Message message) throws CannotArchiveException;
 	
-	public String findMessage(Message message) throws CannotArchiveException;
+	/**
+	 * Searches the message store for a duplicate message.
+	 * If a duplicate is found the unique id for that message is returned.
+	 * If no duplicates are found, null is returned.
+	 * 
+	 * @param message
+	 * @return unique identifier for message
+	 */
+	public String findMessage(Message message);
 	
 }
