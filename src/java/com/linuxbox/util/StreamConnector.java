732dd95f59801796100a4497296c5987f89a00ff
Eric I <eric@linuxbox.com>
2011-04-20 14:03:01 -0400

    Bring in some utility classes.

---
+++ src/java/com/linuxbox/util/StreamConnector.java
@@ -0,0 +1,125 @@
+package com.linuxbox.util;
+/*
+ *  Copyright 2010 The Linux Box Corporation.
+ *
+ *  This file is part of Enkive CE (Community Edition).
+ *
+ *  Enkive CE is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  Enkive CE is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public
+ *  License along with Enkive CE. If not, see
+ *  <http://www.gnu.org/licenses/>.
+ */
+
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class StreamConnector {
+	private final static int BUFFER_SIZE_DEFAULT = 4096;
+
+	/**
+	 * Asynchronously transfers data from an InputStream to an OutputStream
+	 * until the end of the InputStream is reached. Returns a
+	 * StreamConnectorStatus that can be queried to note the progress and
+	 * problems of the asynchronous transfer. The InputStream is closed when its
+	 * end is reached.
+	 * 
+	 * @param input
+	 *            the InputStream
+	 * @param output
+	 *            the OutputStream
+	 * @return a StreamConnectorStatus that can be queried to determine the
+	 *         progress and error-state of the transfer
+	 */
+	public static StreamConnectorStatus transferBackground(InputStream input,
+			OutputStream output) {
+		StreamConnectorStatusImpl result = new StreamConnectorStatusImpl(input,
+				output, BUFFER_SIZE_DEFAULT);
+		result.start();
+		return result;
+	}
+
+	/**
+	 * Transfers data from an InputStream to an OutputStream until the end of
+	 * the InputStream is reached. The InputStream is closed when its end is
+	 * reached.
+	 * 
+	 * @param input
+	 *            the InputStream
+	 * @param output
+	 *            the OutputStream
+	 * @throws IOException
+	 */
+	public static void transferForeground(InputStream input, OutputStream output)
+			throws IOException {
+		byte[] buffer = new byte[BUFFER_SIZE_DEFAULT];
+		int bytesRead;
+		while ((bytesRead = input.read(buffer)) != -1) {
+			output.write(buffer, 0, bytesRead);
+		}
+		input.close();
+	}
+
+	static class StreamConnectorStatusImpl extends Thread implements
+			StreamConnectorStatus {
+		private InputStream input;
+		private OutputStream output;
+		private int bufferSize;
+
+		private boolean finished;
+		private int bytesTransferred;
+		private IOException ioException;
+
+		StreamConnectorStatusImpl(InputStream input, OutputStream output,
+				int bufferSize) {
+			this.input = input;
+			this.output = output;
+			this.bufferSize = bufferSize;
+			finished = false;
+			bytesTransferred = 0;
+			ioException = null;
+		}
+
+		public int getBytesTransferred() {
+			return bytesTransferred;
+		}
+
+		public boolean isFinished() throws IOException {
+			if (ioException != null) {
+				throw ioException;
+			}
+			return finished;
+		}
+
+		public void waitToFinish() throws InterruptedException {
+			this.join();
+		}
+
+		public void run() {
+			try {
+				byte[] buffer = new byte[bufferSize];
+				int bytesRead;
+				while ((bytesRead = input.read(buffer)) != -1) {
+					output.write(buffer, 0, bytesRead);
+					bytesTransferred += bytesRead;
+				}
+				input.close();
+			} catch (IOException e) {
+				ioException = e;
+			} finally {
+				finished = true;
+			}
+		}
+	}
+}
---
+++ src/java/com/linuxbox/util/StringUtils.java
@@ -0,0 +1,83 @@
+/*
+ *  Copyright 2010 The Linux Box Corporation.
+ *
+ *  This file is part of Enkive CE (Community Edition).
+ *
+ *  Enkive CE is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  Enkive CE is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public
+ *  License along with Enkive CE. If not, see
+ *  <http://www.gnu.org/licenses/>.
+ */
+
+package com.linuxbox.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class StringUtils {
+	// private final static Log logger = LogFactory.getLog("com.linuxbox.util");
+
+	public static final boolean isEmpty(String s) {
+		return s == null || s.isEmpty();
+	}
+
+	public static final boolean hasData(String s) {
+		return s != null && !s.isEmpty();
+	}
+
+	public static String collectionToString(Collection<String> collection,
+			String elementSeparator) {
+		StringBuffer s = new StringBuffer();
+		for (Iterator<String> i = collection.iterator(); i.hasNext();) {
+			String s2 = i.next();
+			s.append(s2);
+			if (i.hasNext()) {
+				s.append(elementSeparator);
+			}
+		}
+		return s.toString();
+	}
+
+	public static String collectionToCommaSeparatedString(List<String> list) {
+		return collectionToString(list, ", ");
+	}
+
+	public static String stringFromInputStream(InputStream input)
+			throws IOException {
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		StreamConnector.transferForeground(input, output);
+		try {
+			output.close();
+		} catch (IOException e) {
+			// empty
+		}
+		return output.toString();
+	}
+
+	public static void main(String[] args) {
+		String s1 = null;
+		String s2 = "";
+		String s3 = " ";
+
+		System.out.println(isEmpty(s1));
+		System.out.println(isEmpty(s2));
+		System.out.println(isEmpty(s3));
+
+		System.out.println(hasData(s1));
+		System.out.println(hasData(s2));
+		System.out.println(hasData(s3));
+	}
+}
