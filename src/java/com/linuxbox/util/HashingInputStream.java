69fe169a74177c657ad8a70bda9a0e8ed9b35437
Eric I <eric@linuxbox.com>
2011-05-03 14:30:24 -0400

    Add HashingInputStream to utility package. Add JUnit tests to it as well.

---
+++ src/java/com/linuxbox/util/HashingInputStream.java
@@ -0,0 +1,92 @@
+package com.linuxbox.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.commons.codec.binary.Hex;
+
+public class HashingInputStream extends InputStream {
+	private final static int BUFFER_SIZE = 4 * 1024;
+
+	private MessageDigest digest;
+	private InputStream actualInputStream;
+	private byte[] digestBytes;
+	private String digestString;
+
+	public HashingInputStream(MessageDigest digest, InputStream actualInputStream) {
+		this.digest = digest;
+		this.actualInputStream = actualInputStream;
+	}
+
+	public HashingInputStream(String hashAlgorithm, InputStream actualInputStream)
+			throws NoSuchAlgorithmException {
+		this.digest = MessageDigest.getInstance(hashAlgorithm);
+		this.actualInputStream = actualInputStream;
+	}
+	
+	public String getDigest() {
+		if (digestBytes == null) {
+			digestBytes = digest.digest();
+			digestString = new String((new Hex()).encode(digestBytes));
+		}
+		return digestString;
+	}
+
+	@Override
+	public int available() throws IOException {
+		return actualInputStream.available();
+	}
+
+	@Override
+	public void close() throws IOException {
+		actualInputStream.close();
+	}
+
+	@Override
+	public boolean markSupported() {
+		return false;
+	}
+
+	@Override
+	public int read() throws IOException {
+		int result = actualInputStream.read();
+		if (result >= 0) {
+			digest.update((byte) result);
+		}
+		return result;
+	}
+
+	@Override
+	public int read(byte[] buffer, int offset, int length) throws IOException {
+		int bytesRead = actualInputStream.read(buffer, offset, length);
+		if (bytesRead > 0) {
+			digest.update(buffer, offset, bytesRead);
+		}
+		return bytesRead;
+	}
+
+	@Override
+	public int read(byte[] b) throws IOException {
+		return read(b, 0, b.length);
+	}
+
+	@Override
+	public long skip(long toSkip) throws IOException {
+		byte[] buffer = new byte[BUFFER_SIZE];
+
+		long totalRead = 0;
+		int result;
+		do {
+			int toRead = (toSkip - totalRead > BUFFER_SIZE) ? BUFFER_SIZE
+					: (int) (toSkip - totalRead);
+			result = read(buffer, 0, toRead); // computes digest on its own
+			if (result > 0) {
+				totalRead += result;
+			}
+		} while (totalRead < toSkip && result >= 0);
+
+		return totalRead;
+	}
+}
---
+++ test/java/com/linuxbox/util/HashingInputStreamTest.java
@@ -0,0 +1,99 @@
+package com.linuxbox.util;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+
+import org.apache.commons.codec.binary.Hex;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HashingInputStreamTest {
+	static String HASH_ALGORITHM = "SHA-1";
+	static byte[] message;
+	static String digestReference;
+
+	HashingInputStream input;
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		for (int b = 1; b <= 255; b++) {
+			for (int count = 1; count <= b; count++) {
+				out.write((byte) b);
+			}
+		}
+		message = out.toByteArray();
+		out.close();
+
+		MessageDigest md = MessageDigest.getInstance(HASH_ALGORITHM);
+		md.update(message);
+		byte[] digestBytes = md.digest();
+		digestReference = new String((new Hex()).encode(digestBytes));
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		input = new HashingInputStream(HASH_ALGORITHM,
+				new ByteArrayInputStream(message));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		input.close();
+	}
+
+	@Test
+	public void testRead() throws IOException {
+		while (input.read() >= 0) {
+			// do nothing
+		}
+		input.close();
+		String digest = input.getDigest();
+		assertEquals("testing equality of digests", digestReference, digest);
+	}
+
+	@Test
+	public void testReadByteArray() throws IOException {
+		byte[] buffer = new byte[4 * 1024];
+		while (input.read(buffer) > 0) {
+			// do nothing
+		}
+		String digest = input.getDigest();
+		assertEquals("testing equality of digests", digestReference, digest);
+	}
+
+	@Test
+	public void testReadByteArrayIntInt() throws IOException {
+		final int start = 17;
+		final int len = 3433; // prime number
+		byte[] buffer = new byte[4 * 1024];
+		while (input.read(buffer, start, len) > 0) {
+			// do nothing
+		}
+		String digest = input.getDigest();
+		assertEquals("testing equality of digests", digestReference, digest);
+	}
+
+	@Test
+	public void testSkip() throws IOException {
+		byte[] buffer = new byte[512];
+		input.read(buffer);
+		input.skip(2 * buffer.length);
+		while (input.read(buffer) > 0) {
+			// do nothing
+		}
+		String digest = input.getDigest();
+		assertEquals("testing equality of digests", digestReference, digest);
+	}
+
+	@Test
+	public void testMarkSupported() {
+		assertFalse(input.markSupported());
+	}
+}
