78f4486116a607b0e56aa8d40e8b131a07163363
Lee Dobryden <lee@linuxbox.com>
2011-05-23 12:36:40 -0400

    Add archiver and retriever tests to test suite

--- test/java/com/linuxbox/enkive/AllTests.java
+++ test/java/com/linuxbox/enkive/AllTests.java
@@ -6,8 +6,10 @@ import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import com.linuxbox.enkive.archiver.mongodb.MongoArchivingServiceTest;
 import com.linuxbox.enkive.docstore.AbstractDocStoreServiceTest;
 import com.linuxbox.enkive.docstore.mongogrid.MongoGridDocStoreServiceTest;
+import com.linuxbox.enkive.retriever.mongodb.MongoRetrieverServiceTest;
 import com.linuxbox.util.HashingInputStreamTest;
 import com.linuxbox.util.mongodb.MongoLockingServiceTest;
 
@@ -19,7 +21,9 @@ import com.linuxbox.util.mongodb.MongoLockingServiceTest;
   HashingInputStreamTest.class,
   AbstractDocStoreServiceTest.class,
   MongoLockingServiceTest.class,
-  MongoGridDocStoreServiceTest.class
+  MongoGridDocStoreServiceTest.class,
+  MongoArchivingServiceTest.class,
+  MongoRetrieverServiceTest.class
 })
 public class AllTests {
 	public static Test suite() {
