1430eeba5f03b0e45979b5e7123d77458c48b9ea
Eric I <eric@linuxbox.com>
2011-06-24 13:16:55 -0400

    Fix non-webapp version to use TestServlet.

--- src/java/com/linuxbox/enkive/MainJetty.java
+++ src/java/com/linuxbox/enkive/MainJetty.java
@@ -1,45 +1,18 @@
 package com.linuxbox.enkive;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.springframework.context.ApplicationContext;
 
+import com.linuxbox.enkive.web.TestServlet;
+
 public class MainJetty extends MainConsole {
-	public static class TempServlet extends HttpServlet {
-		private static final long serialVersionUID = -6875852203346145792L;
-
-		@Override
-		public void init() throws ServletException {
-			super.init();
-		}
-
-		@Override
-		public void init(ServletConfig config) throws ServletException {
-			super.init(config);
-		}
-
-		public void doGet(HttpServletRequest req, HttpServletResponse resp)
-				throws IOException {
-			PrintWriter out = resp.getWriter();
-			out.println("<html><body>Hello, <b>Eric</b>.</body></html>");
-		}
-	}
-
 	static final String[] CONFIG_FILES = { "jetty-server.xml" };
 
 	public MainJetty(String[] arguments) {
 		this(CONFIG_FILES, arguments);
 	}
-	
+
 	public MainJetty(String[] configFiles, String[] arguments) {
 		super(configFiles, arguments);
 	}
@@ -59,8 +32,8 @@ public class MainJetty extends MainConsole {
 				"ServletContextHandler",
 				org.eclipse.jetty.servlet.ServletContextHandler.class);
 
-		servletContextHandler.addServlet(TempServlet.class, "/test");
-
+		servletContextHandler.addServlet(TestServlet.class, "/test");
+		
 		super.doEventLoop(context);
 	}
 
