bd84ed46db904801ab6a6910db05364ad064a087
Eric I <eric@linuxbox.com>
2011-06-24 12:11:53 -0400

    Get simple servlets up and running.

--- src/java/com/linuxbox/enkive/MainJetty.java
+++ src/java/com/linuxbox/enkive/MainJetty.java
@@ -1,32 +1,66 @@
 package com.linuxbox.enkive;
 
-import org.eclipse.jetty.deploy.providers.ContextProvider;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.springframework.context.ApplicationContext;
 
 public class MainJetty extends MainConsole {
+	public static class TempServlet extends HttpServlet {
+		private static final long serialVersionUID = -6875852203346145792L;
+
+		@Override
+		public void init() throws ServletException {
+			super.init();
+		}
+
+		@Override
+		public void init(ServletConfig config) throws ServletException {
+			super.init(config);
+		}
+
+		public void doGet(HttpServletRequest req, HttpServletResponse resp)
+				throws IOException {
+			PrintWriter out = resp.getWriter();
+			out.println("<html><body>Hello, <b>Eric</b>.</body></html>");
+		}
+	}
+
 	static final String[] CONFIG_FILES = { "jetty-server.xml" };
 
 	public MainJetty(String[] arguments) {
-		super(CONFIG_FILES, arguments);
+		this(CONFIG_FILES, arguments);
+	}
+	
+	public MainJetty(String[] configFiles, String[] arguments) {
+		super(configFiles, arguments);
 	}
 
 	@Override
 	protected void startup() {
 		super.startup();
-		out.println("Jetty started");
+		out.println("Jetty will be starting...");
 	}
 
 	protected void doEventLoop(ApplicationContext context) {
-		org.mortbay.jetty.spring.Server server = context.getBean("Server",
-				org.mortbay.jetty.spring.Server.class);
-		ContextProvider provider = server.getBean(ContextProvider.class);
+		@SuppressWarnings("unused")
+		Server server = context.getBean("Server",
+				org.eclipse.jetty.server.Server.class);
 
-		/*
-		 * ContextProvider provider = context.getBean("ContextProvider",
-		 * ContextProvider.class);
-		 */
+		ServletContextHandler servletContextHandler = context.getBean(
+				"ServletContextHandler",
+				org.eclipse.jetty.servlet.ServletContextHandler.class);
 
-		// provider
+		servletContextHandler.addServlet(TempServlet.class, "/test");
+
 		super.doEventLoop(context);
 	}
 
