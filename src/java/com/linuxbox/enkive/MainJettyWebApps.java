8282031ac2606821af1818fde7008578391ffe72
Eric I <eric@linuxbox.com>
2011-06-24 12:15:38 -0400

    Try to get Jetty to recognize webapps and have access to all Enkive classes.

---
+++ src/java/com/linuxbox/enkive/MainJettyWebApps.java
@@ -0,0 +1,37 @@
+package com.linuxbox.enkive;
+
+import org.eclipse.jetty.server.Server;
+import org.springframework.context.ApplicationContext;
+
+
+public class MainJettyWebApps extends MainConsole {
+	static final String[] CONFIG_FILES = { "jetty-server-webapps.xml" };
+
+	public MainJettyWebApps(String[] arguments) {
+		super(CONFIG_FILES, arguments);
+	}
+	
+	@Override
+	protected void startup() {
+		super.startup();
+		out.println("Jetty will be starting...");
+	}
+	
+	protected void doEventLoop(ApplicationContext context) {
+		@SuppressWarnings("unused")
+		Server server = context.getBean("Server",
+				org.eclipse.jetty.server.Server.class);
+
+		super.doEventLoop(context);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO attempt to change classpath
+		// System.setProperty("jetty.class.path", "./bin/java");
+		Main main = new MainJettyWebApps(args);
+		main.run();
+	}
+}
