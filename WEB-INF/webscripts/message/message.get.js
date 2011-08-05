84483f6b36e2b5a27570c5814c4b9b41fb156cf1
Lee N Dobryden <lee@linuxbox.com>
2011-08-05 15:14:30 -0400

    Adding frontend support for attachment download

--- WEB-INF/webscripts/message/message.get.js
+++ WEB-INF/webscripts/message/message.get.js
@@ -1,10 +1,16 @@
 var messageid = context.properties["messageid"];
 // get a connector to the Alfresco repository endpoint
 var connector = remote.connect("alfresco");
+
+var attachments = connector.get("/enkive/messageattachments/" + messageid);
+
 // retrieve the message
 var response = connector.get("/enkive/message/" + messageid);
 
-if(response.status == 200)
+if(response.status == 200){
 	model.text = response;
+	var attachmentsJSON = eval("(" + attachments + ")");
+	model.attachments = attachmentsJSON.data;
+}
 else
 	model.text = "Your session has expired.  You must login to view this message";
