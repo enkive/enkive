1eb6a7478667036a0423d4af3a8a933f35b3ce77
Lee N Dobryden <lee@linuxbox.com>
2012-02-18 08:27:39 -0500

    fix potential bug in javascript

--- WEB-INF/webscripts/message/message.get.js
+++ WEB-INF/webscripts/message/message.get.js
@@ -3,12 +3,12 @@ var connector = remote.connect("enkive");
 
 // retrieve the message
 var response = connector.get("/message/retrieve?message_id=" + messageid);
-var attachments = connector.get("/message/attachmentDetail?message_id=" + messageid);
 
-if(response.status == 200){
+if (response.status == 200) {
+	var attachments = connector.get("/message/attachmentDetail?message_id="
+			+ messageid);
 	model.text = response;
 	var attachmentsJSON = eval("(" + attachments + ")");
 	model.attachments = attachmentsJSON.data;
-}
-else
+} else
 	model.text = "Your session has expired.  You must login to view this message";
