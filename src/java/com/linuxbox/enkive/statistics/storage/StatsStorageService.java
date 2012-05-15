8a7ac09264034a4ac382b6b1fef772bfffa42c74
Eric I <eric@linuxbox.com>
2012-05-15 17:31:02 -0400

    Change declaration of methods in StatsStorageService.

--- src/java/com/linuxbox/enkive/statistics/storage/StatsStorageService.java
+++ src/java/com/linuxbox/enkive/statistics/storage/StatsStorageService.java
@@ -1,12 +1,13 @@
 package com.linuxbox.enkive.statistics.storage;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 public interface StatsStorageService {
 	void storeStatistics(String service, Date timestamp,
-			Map<String, String> data) throws StatsStorageException;
+			Map<String, Object> data) throws StatsStorageException;
 
-	void queryStatistics(String statsName, Date startingTimestamp,
+	List<Object> queryStatistics(String statsName, Date startingTimestamp,
 			Date endingTimestamp) throws StatsStorageException;
 }
--- src/java/com/linuxbox/enkive/statistics/storage/mongodb/MongoStatsStorageService.java
+++ src/java/com/linuxbox/enkive/statistics/storage/mongodb/MongoStatsStorageService.java
@@ -1,6 +1,7 @@
 package com.linuxbox.enkive.statistics.storage.mongodb;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import com.linuxbox.enkive.statistics.storage.StatsStorageException;
@@ -9,20 +10,19 @@ import com.linuxbox.enkive.statistics.storage.StatsStorageService;
 public class MongoStatsStorageService implements StatsStorageService {
 
 	/* NOAH: you'll need to fill this out */
-	
-	
+
 	@Override
 	public void storeStatistics(String service, Date timestamp,
-			Map<String, String> data) throws StatsStorageException {
+			Map<String, Object> data) throws StatsStorageException {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
-	public void queryStatistics(String statsName, Date startingTimestamp,
-			Date endingTimestamp) throws StatsStorageException {
+	public List<Object> queryStatistics(String statsName,
+			Date startingTimestamp, Date endingTimestamp)
+			throws StatsStorageException {
 		// TODO Auto-generated method stub
-
+		return null;
 	}
 
 }
