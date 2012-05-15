b57b11bb625d66ce6289123ba5b84d1b55d3d21d
Eric I <eric@linuxbox.com>
2012-05-15 18:24:24 -0400

    Add some more examples/comments for Noah.

--- src/java/com/linuxbox/enkive/statistics/storage/StatsStorageService.java
+++ src/java/com/linuxbox/enkive/statistics/storage/StatsStorageService.java
@@ -8,6 +8,15 @@ public interface StatsStorageService {
 	void storeStatistics(String service, Date timestamp,
 			Map<String, Object> data) throws StatsStorageException;
 
-	List<Object> queryStatistics(String statsName, Date startingTimestamp,
+	/**
+	 * Does a query on the statistics service back end.
+	 * 
+	 * @param statName A dot-separated path to the statistic desired starting with the service name (e.g., "MessageStorageService.count", "AuditService.count.max")
+	 * @param startingTimestamp
+	 * @param endingTimestamp
+	 * @return a list of results. Each result could be a simple object (e.g., String, Integer) or something more complex (List, Map).
+	 * @throws StatsStorageException
+	 */
+	List<Object> queryStatistics(String statName, Date startingTimestamp,
 			Date endingTimestamp) throws StatsStorageException;
 }
--- src/java/com/linuxbox/enkive/statistics/storage/mongodb/MongoStatsStorageService.java
+++ src/java/com/linuxbox/enkive/statistics/storage/mongodb/MongoStatsStorageService.java
@@ -6,10 +6,27 @@ import java.util.Map;
 
 import com.linuxbox.enkive.statistics.storage.StatsStorageException;
 import com.linuxbox.enkive.statistics.storage.StatsStorageService;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
 
 public class MongoStatsStorageService implements StatsStorageService {
+	/*
+	 * NOAH: feel free to change the name or to put it in a special Mongo
+	 * constants interface/class for the MongoStatsStorageService
+	 */
+	static final String STATS_COLLECTION = "stats";
+
+	private DBCollection statsCollection;
 
 	/* NOAH: you'll need to fill this out */
+
+	public MongoStatsStorageService(Mongo mongo, String dbName) {
+		final DB mongoDB = mongo.getDB(dbName);
+
+		statsCollection = mongoDB.getCollection(STATS_COLLECTION);
+	}
 
 	@Override
 	public void storeStatistics(String service, Date timestamp,
@@ -18,11 +35,19 @@ public class MongoStatsStorageService implements StatsStorageService {
 	}
 
 	@Override
-	public List<Object> queryStatistics(String statsName,
+	public List<Object> queryStatistics(String statName,
 			Date startingTimestamp, Date endingTimestamp)
 			throws StatsStorageException {
 		// TODO Auto-generated method stub
-		return null;
+		try {
+			// do something with the Mongo back-end
+		} catch (MongoException e) {
+			// here we've caught one kind of exception and turned it into
+			// another; we could have also done ''new
+			// StatsStorageException("some message", e)''
+			throw new StatsStorageException(e);
+		}
+		return null; // auto-generated
 	}
 
 }
