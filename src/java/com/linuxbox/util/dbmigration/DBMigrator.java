55fc5f57d4ceebf3f3ecf84e8a591686f5048cee
Eric I <eric@linuxbox.com>
2012-11-30 15:07:12 -0500

    Laying out structure for a generic DB migration capability.

---
+++ src/java/com/linuxbox/util/dbmigration/DBMigrator.java
@@ -0,0 +1,57 @@
+package com.linuxbox.util.dbmigration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DBMigrator {
+	Map<Integer, DBMigration> migrations = new HashMap<Integer, DBMigration>();
+	String migratorName;
+	DBInfo db;
+
+	public DBMigrator(String migratorName, DBInfo db) {
+		this.migratorName = migratorName;
+		this.db = db;
+	}
+
+	public void registerMigration(DBMigration migration)
+			throws DBMigrationException {
+		if (migrations.containsValue(migration.fromVersion)) {
+			throw new DBMigrationException("already have migration for "
+					+ migratorName + " from version " + migration.fromVersion);
+		}
+		migrations.put(migration.fromVersion, migration);
+	}
+
+	public int runThrough(int fromVersion, int toVersion)
+			throws DBMigrationException {
+		while (fromVersion < toVersion) {
+			Integer newVersion = runNext(fromVersion);
+			if (newVersion == null) {
+				throw new DBMigrationException(
+						"Noah, please put something here");
+			}
+			fromVersion = newVersion;
+		}
+		return toVersion;
+	}
+
+	public int runAll(int fromVersion) throws DBMigrationException {
+		Integer lastVersion = fromVersion;
+		Integer newVersion = runNext(lastVersion);
+		while (newVersion != null) {
+			lastVersion = newVersion;
+			newVersion = runNext(lastVersion);
+		}
+		return lastVersion;
+	}
+
+	Integer runNext(int fromVersion) throws DBMigrationException {
+		DBMigration nextMigration = migrations.get(fromVersion);
+		if (nextMigration == null) {
+			return null;
+		} else {
+			nextMigration.migrate(db);
+			return nextMigration.toVersion;
+		}
+	}
+}
---
+++ src/java/com/linuxbox/util/dbmigration/mongodb/MongoDBInfo.java
@@ -0,0 +1,38 @@
+package com.linuxbox.util.dbmigration.mongodb;
+
+import com.linuxbox.util.dbmigration.DBInfo;
+import com.linuxbox.util.dbmigration.DBMigrationException;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+
+// NOAH: this might be incomplete; you might need something like the db name or collection name in order to create a secondary, temporary collection in which to place the new data
+
+public class MongoDBInfo implements DBInfo {
+	Mongo mongo;
+	String dbName;
+	DBCollection collection;
+
+	public MongoDBInfo(Mongo mongo, String dbName, DBCollection collection) {
+		this.mongo = mongo;
+		this.dbName = dbName;
+		this.collection = collection;
+	}
+
+	public Mongo getMongo() {
+		return mongo;
+	}
+
+	public String getDbName() {
+		return dbName;
+	}
+
+	public DBCollection getCollection() {
+		return collection;
+	}
+
+	@Override
+	public int getCurrentVersion() throws DBMigrationException {
+		// TODO needs to call into the DB to get the current version number
+		throw new DBMigrationException("unimplemented");
+	}
+}
