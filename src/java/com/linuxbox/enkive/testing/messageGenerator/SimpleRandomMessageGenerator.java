a723b4797bd6651bf2fc443fefd64de953c6d76a
Eric I <ivancich@gmail.com>
2013-01-23 18:58:07 -0800

    Get rid of warning that BufferedReaders were not being closed. Remove duplicated code.

--- src/java/com/linuxbox/enkive/testing/messageGenerator/SimpleRandomMessageGenerator.java
+++ src/java/com/linuxbox/enkive/testing/messageGenerator/SimpleRandomMessageGenerator.java
@@ -1,22 +1,3 @@
-/*******************************************************************************
- * Copyright 2012 The Linux Box Corporation.
- *
- * This file is part of Enkive CE (Community Edition).
- *
- * Enkive CE is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *
- * Enkive CE is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public
- * License along with Enkive CE. If not, see
- * <http://www.gnu.org/licenses/>.
- *******************************************************************************/
 package com.linuxbox.enkive.testing.messageGenerator;
 
 import java.io.BufferedReader;
@@ -27,12 +8,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Random;
 
-public class SimpleRandomMessageGenerator extends AbstractMessageGenerator {
-
+public class SimpleRandomMessageGenerator extends
+		AbstractMessageGenerator {
+	
 	protected Random randGen;
 	protected String messageBodyDataDirectory;
 
 	public SimpleRandomMessageGenerator(String messageBodyDataDirectory) {
+		super();
 		randGen = new Random();
 		this.messageBodyDataDirectory = messageBodyDataDirectory;
 	}
@@ -44,25 +27,26 @@ public class SimpleRandomMessageGenerator extends AbstractMessageGenerator {
 		try {
 			File messageBodyDataDir = new File(messageBodyDataDirectory);
 			File[] files = messageBodyDataDir.listFiles();
-
+	
 			int fileToGet = randGen.nextInt(files.length);
 			is = new FileInputStream(files[fileToGet]);
-
+	
 			BufferedReader dis = new BufferedReader(new InputStreamReader(is));
+			final String lineSeparator = System.getProperty("line.separator");
 			String s;
 			while ((s = dis.readLine()) != null) {
-				messageBody.append(s + System.getProperty("line.separator"));
+				messageBody.append(s + lineSeparator);
 			}
-
+			dis.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				is.close();
 			} catch (IOException ioe) {
-
+	
 			}
-
+	
 		}
 		return messageBody.toString();
 	}
