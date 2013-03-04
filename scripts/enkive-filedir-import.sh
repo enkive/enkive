ac55db0bd9d026c6d0c13606283c749372d449a9
Eric I <eric@linuxbox.com>
2013-03-04 11:58:32 -0500

    Add some scripts to aid in the importing of email messages.

---
+++ scripts/enkive-filedir-import.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# export ENKIVE_CONSOLE_LOGGING=full
+export ENKIVE_MAIN=com.linuxbox.enkive.importer.FileDirReader
+
+if [ $# -eq 0 ] ;then
+    echo "Usage: $0 email-directory [host [port]]"
+    echo "    default host is localhost"
+    echo "    default port is 2526"
+    exit 1
+fi
+
+host=localhost
+port=2526
+
+directory=$1
+
+if [ $# -ge 2 ] ;then
+    host="$2"
+fi
+
+if [ $# -ge 3 ] ;then
+    port="$3"
+fi
+
+sh $(dirname $0)/enkive-common.sh $directory $host $port
