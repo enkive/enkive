3ea7b78812a38f2eaf64e1239be739b23cd6c740
Eric I <eric@linuxbox.com>
2013-03-12 18:09:38 -0400

    Remove warning.

--- test/java/com/linuxbox/enkive/docstore/mongogrid/MongoGridDocStoreServiceTest.java
+++ test/java/com/linuxbox/enkive/docstore/mongogrid/MongoGridDocStoreServiceTest.java
@@ -28,7 +28,6 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.security.MessageDigest;
 import java.util.UUID;
 
