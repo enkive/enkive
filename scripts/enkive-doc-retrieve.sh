9a04deed6cf9e5ec86b4f456ac4ced3147ad6c23
Eric I <eric@linuxbox.com>
2013-03-15 15:39:03 -0400

    Rename and move the MongoDBDocRetriever to the tool package. Add ability to run Apache Tika over documents to see what Indri sees.

---
+++ scripts/enkive-doc-retrieve.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# export ENKIVE_CONSOLE_LOGGING=full
+export ENKIVE_MAIN=com.linuxbox.enkive.tool.mongodb.MongoDBDocRetriever
+
+if [ $# -ne 1 ] ;then
+    echo "Usage: $0 document-id"
+    exit 1
+fi
+
+docid=$1
+
+sh $(dirname $0)/enkive-common.sh $docid
