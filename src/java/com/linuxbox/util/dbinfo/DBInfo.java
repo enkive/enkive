ade1a47c168b0bb02fc871df441292c9ef53d736
Eric I <eric@linuxbox.com>
2013-04-26 17:57:51 -0400

    Document use of DBInfo.

--- src/java/com/linuxbox/util/dbinfo/DBInfo.java
+++ src/java/com/linuxbox/util/dbinfo/DBInfo.java
@@ -1,5 +1,11 @@
 package com.linuxbox.util.dbinfo;
 
+/**
+ * Encapsulates the access to a database. We need to separate this from the
+ * services themselves because the migrators also need them. And rather than
+ * doing it twice it'd be better to do it once in a Spring configuration XML
+ * file that gets loaded by the services and by the migrators.
+ */
 public interface DBInfo {
 	public String getServiceName();
 }
