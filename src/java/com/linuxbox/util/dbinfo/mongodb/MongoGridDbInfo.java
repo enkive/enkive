bbf66c0026a5e3814c15d6ba363adda703727f8f
Eric I <eric@linuxbox.com>
2013-05-01 12:16:43 -0400

    First stab at using DBInfo objects to simplify sharing of DB information between services and migrators.

---
+++ src/java/com/linuxbox/util/dbinfo/mongodb/MongoGridDbInfo.java
@@ -0,0 +1,24 @@
+package com.linuxbox.util.dbinfo.mongodb;
+
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import com.mongodb.gridfs.GridFS;
+
+public class MongoGridDbInfo extends MongoDBInfo {
+	final protected GridFS gridFs;
+
+	public MongoGridDbInfo(String serviceName, Mongo mongo, String dbName,
+			String bucketName) {
+		this(serviceName, mongo, mongo.getDB(dbName), bucketName);
+	}
+	
+	public MongoGridDbInfo(String serviceName, Mongo mongo, DB db,
+			String bucketName) {
+		super(serviceName, mongo, db, bucketName);
+		this.gridFs = new GridFS(db, bucketName);
+	}
+
+	public GridFS getGridFs() {
+		return gridFs;
+	}
+}
