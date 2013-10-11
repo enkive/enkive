8242e12b0f93b1094875a5a18054a33aebc71d13
Daniel Gryniewicz <dang@linuxbox.com>
2013-10-11 11:24:24 -0400

    Update copyright notice on webapp to include AGPL

--- jetty/webapps/ediscovery/WEB-INF/webscripts/footer/footer.get.js
+++ jetty/webapps/ediscovery/WEB-INF/webscripts/footer/footer.get.js
@@ -1,2 +1,2 @@
 model.message = '<a class="footer_link" href="http://www.enkive.org">Enkive</a> is the email archiving solution made by <a class="footer_link" href="http://www.linuxbox.com">The Linux Box</a>.';
-model.copyright = 'Copyright © 2013 The Linux Box Corporation. All rights reserved. Enkive® is a registered trademark of The Linux Box Corporation.';
+model.copyright = 'Copyright © 2013 The Linux Box Corporation. Released under the GNU AGPL version 3. Enkive® is a registered trademark of The Linux Box Corporation.';
